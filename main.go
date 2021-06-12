7c8951e9ac31df330bfa6c3632b116e49b155525
moeabdol <mohd.a.saed@gmail.com>
2021-06-12 04:34:50 +0300

    Refactor routes

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/moeabdol/bookstore-api-golang/controllers"
 	db "github.com/moeabdol/bookstore-api-golang/db/sqlc"
-	"github.com/moeabdol/bookstore-api-golang/routes"
 	"github.com/moeabdol/bookstore-api-golang/utils"
 )
 
@@ -21,12 +21,19 @@ func main() {
 		utils.Log.Info("Connected to database " + utils.Config.DbName)
 	}
 
-	router := mux.NewRouter()
-	apiSubrouter := router.PathPrefix("/api").Subrouter()
-	routes.InitializeBookRoutes(apiSubrouter)
-	routes.InitializeAuthorRoutes(apiSubrouter)
+	r := mux.NewRouter()
+	r.HandleFunc("/books", controllers.CreateBook).Methods(http.MethodPost)
+	r.HandleFunc("/books", controllers.ListBooks).Methods(http.MethodGet)
+	r.HandleFunc("/books/{id}", controllers.GetBook).Methods(http.MethodGet)
+	r.HandleFunc("/books/{id}", controllers.UpdateBook).Methods(http.MethodPut)
+	r.HandleFunc("/books/{id}", controllers.DeleteBook).Methods(http.MethodDelete)
+	r.HandleFunc("/authors", controllers.CreateAuthor).Methods(http.MethodPost)
+	r.HandleFunc("/authors", controllers.ListAuthors).Methods(http.MethodGet)
+	r.HandleFunc("/authors/{id}", controllers.GetAuthor).Methods(http.MethodGet)
+	r.HandleFunc("/authors/{id}", controllers.UpdateAuthor).Methods(http.MethodPut)
+	r.HandleFunc("/authors/{id}", controllers.DeleteAuthor).Methods(http.MethodDelete)
 	utils.Log.Info("Finished initializing routes")
 
 	utils.Log.Info("Server ready and listening on port " + utils.Config.Port)
-	utils.Log.Fatal(http.ListenAndServe(":"+utils.Config.Port, router))
+	utils.Log.Fatal(http.ListenAndServe(":"+utils.Config.Port, r))
 }
